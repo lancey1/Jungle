5ffde5fcdcd77557c921cbab29f5e75896503626
lancey1 <lanceyu619@gmail.com>
2022-06-30 23:35:36 -0400

    add sale banner and create sales helper

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,11 +1,14 @@
 class Sale < ActiveRecord::Base
+  def self.active
+    Sale.where("starts_on <= ? AND ends_on >= ?", Date.current, Date.current)
+  end
   def finished?
     ends_on < Date.current
   end
   def upcoming?
     starts_on > Date.current
   end
-  def upcoming?
+  def active?
     !finished? && !upcoming?
   end
 end
